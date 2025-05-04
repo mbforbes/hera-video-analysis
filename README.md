7a2449dd37f3402a6f7d05cb8f1cf4bb1653df5a
Max Forbes <m.b.forbes@gmail.com>
2025-05-04 01:00:34 -0400

    several more runs, basic viz script, simple plan (too many files)

---
+++ README.md
@@ -0,0 +1,8 @@
+plan:
+
+-  [ ] use fewer files
+   -  [ ] convert to single file for caching binary search lookup results (age text or numeral)
+   -  [ ] convert existing files to this format
+-  [ ] get more data
+-  [ ] plot vils
+-  [ ] maybe try other plots that might be interesting
--- viz.py
+++ viz.py
@@ -1,14 +1,35 @@
 import os
+from typing import Optional
 
+import matplotlib.pyplot as plt
 from mbforbes_python_utils import read
+import seaborn as sns
 
-from main import AgeClickIndex
+from main import AgeClickIndex, FrameResult, AgeText
 
+# load data
 output_dir = "./output"
 sds = [d for d in os.listdir(output_dir) if os.path.isdir(os.path.join(output_dir, d))]
+villager_counts: list[tuple[AgeText, Optional[int]]] = []
 for sd in sds:
     subdir = os.path.join(output_dir, sd)
     aci_path = os.path.join(subdir, "age_click_index.json")
     if os.path.exists(aci_path):
         aci = AgeClickIndex.model_validate_json(read(aci_path))
-        aci.ageclick2frame  # TODO: curspot
+        for age_text, frame_number in aci.ageclick2frame.items():
+            if frame_number is None:
+                continue
+            frame_result_path = os.path.join(subdir, f"frame_{frame_number}_results.json")
+            frame_result = FrameResult.model_validate_json(read(frame_result_path))
+            villager_counts.append((age_text, frame_result.villagers.total))
+
+# plot
+sns.set_theme(style="darkgrid", rc={"figure.figsize": (10, 6)})
+sns.histplot(
+    x=[v[1] for v in villager_counts],
+    hue=[v[0] for v in villager_counts],
+    bins=100,
+    binrange=(0, 101),
+)
+plt.xlabel("Villagers")
+plt.show()
